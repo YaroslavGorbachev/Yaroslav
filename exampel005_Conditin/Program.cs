0cc6a6067b53240d9ad2ce5382f867c0651e031b
Yaroslav <104135869+YaroslavGorbachev@users.noreply.github.com>
2022-05-09 00:44:31 +0700

    If-Else

---
+++ exampel005_Conditin/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите ваше имя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура,этоже Маша!");
+}
+
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
